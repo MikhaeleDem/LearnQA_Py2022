4e5d193d1591a9acdc9691352cfa4bf08542bc87
mdannikov <m.dannicov@mail.ru>
2022-05-23 17:21:16 +0300

    коммит с историями редиректа

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/hello")
-print(response.text)
+response = requests.post("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
+first_response = response.history[0]
+second_response = response
+print(response.history)
+print(first_response.url)
+print(second_response.url)
+
+
